6ac7a69dcb5beec393efbfd025d974054f8e5ba0
Pranav-Bhosale <pranavbhosale@sahaj.ai>
2023-03-16 16:17:17 +0530

    Format SpotTest

--- src/test/kotlin/SpotTest.kt
+++ src/test/kotlin/SpotTest.kt
@@ -1,23 +1,22 @@
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class SpotTest{
+class SpotTest {
     @Test
-    fun `isAvailable should return true if spot is available`()
-    {
-        val spot=Spot()
+    fun `isAvailable should return true if spot is available`() {
+        val spot = Spot()
 
-        val isAvailable=spot.isAvailable()
+        val isAvailable = spot.isAvailable()
 
-        assertEquals(true,isAvailable)
+        assertEquals(true, isAvailable)
     }
+
     @Test
-    fun `isAvailable should return false if spot is not available`()
-    {
-        val spot=Spot(SpotStatus.BOOKED)
+    fun `isAvailable should return false if spot is not available`() {
+        val spot = Spot(SpotStatus.BOOKED)
 
-        val isAvailable=spot.isAvailable()
+        val isAvailable = spot.isAvailable()
 
-        assertEquals(false,isAvailable)
+        assertEquals(false, isAvailable)
     }
 }
